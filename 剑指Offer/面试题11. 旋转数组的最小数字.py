0a93b44e58515443a15d6b2048bfb912b7bbfaa7
PlutoaCharon <thisishaohan@foxmail.com>
2020-07-08 18:01:51 +0800

    Python刷题记录

---
+++ 剑指Offer/面试题11. 旋转数组的最小数字.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minArray(self, numbers: [int]) -> int:
+        i, j = 0, len(numbers) - 1
+        while i < j:
+            m = (i + j) // 2
+            if numbers[m] > numbers[j]: i = m + 1
+            elif numbers[m] < numbers[j]: j = m
+            else: j -= 1
+        return numbers[i]
