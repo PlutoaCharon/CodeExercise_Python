138e4528578b0ee56bbd5443b834f1515785db9a
PlutoaCharon <thisishaohan@foxmail.com>
2020-07-10 11:35:41 +0800

    Python刷题记录

---
+++ LeetCode/121. 买卖股票的最佳时机.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        minprice = float('inf')
+        maxprofit = 0
+        for price in prices:
+            minprice = min(minprice, price)
+            maxprofit = max(maxprofit, price - minprice)
+        return maxprofit
--- LeetCode/714. 买卖股票的最佳时机含手续费.py
+++ LeetCode/714. 买卖股票的最佳时机含手续费.py
@@ -1,10 +1,8 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
         dp_i_0, dp_i_1 = 0, float("-inf")
-        dp_pre_0 = 0
         for price in prices:
             tmp = dp_i_0
             dp_i_0 = max(dp_i_0, dp_i_1 + price)
-            dp_i_1 = max(dp_i_1, dp_pre_0 - price)
-            dp_pre_0 = tmp
+            dp_i_1 = max(dp_i_1, tmp - price - fee)
         return dp_i_0
