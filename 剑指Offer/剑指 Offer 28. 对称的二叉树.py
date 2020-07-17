dcc74262fa9f09e7d42af2046c3314bb06a9af5c
PlutoaCharon <thisishaohan@foxmail.com>
2020-07-17 19:25:04 +0800

    Python刷题记录

---
+++ 剑指Offer/剑指 Offer 28. 对称的二叉树.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        # 特判
+        if not root:
+            return True
+
+        def recur(L, R):
+            if not L and not R:
+                return True
+            if not L or not R or L.val != R.val:
+                return False
+            return recur(L.left, R.right) and recur(L.right, R.left)
+
+        return recur(root.left, root.right)
+
