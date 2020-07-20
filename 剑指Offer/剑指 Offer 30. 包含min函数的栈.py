8579943d7dde144d7912c683a604dabf0fbeb5e0
PlutoaCharon <thisishaohan@foxmail.com>
2020-07-20 15:18:20 +0800

    Python刷题记录

---
+++ 剑指Offer/剑指 Offer 30. 包含min函数的栈.py
@@ -0,0 +1,31 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        # A维护push，pop，top函数
+        # B维护min函数
+        self.A, self.B = [], []
+
+    def push(self, x: int) -> None:
+        self.A.append(x)
+        if not self.B or self.B[-1] >= x:
+            self.B.append(x)
+
+    def pop(self) -> None:
+        if self.A.pop() == self.B[-1]:
+            self.B.pop()
+
+    def top(self) -> int:
+        return self.A[-1]
+
+    def min(self) -> int:
+        return self.B[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.min()
