b8a48647e6f7f0edc8872aa96502d358f5717708
PlutoaCharon <thisishaohan@foxmail.com>
2020-07-23 18:57:06 +0800

    Python刷题记录

---
+++ 剑指Offer/剑指 Offer 31. 栈的压入、弹出序列.py
@@ -0,0 +1,9 @@
+class Solution:
+    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        stack, i = [], 0
+        for num in pushed:
+            stack.append(num) # num 入栈
+            while stack and stack[-1] == popped[i]: # 循环判断与出栈
+                stack.pop()
+                i += 1
+        return not stack
