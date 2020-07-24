415f05f6ff1d8d89c3d549a76cc4683677a86188
PlutoaCharon <thisishaohan@foxmail.com>
2020-07-24 19:27:15 +0800

    Python刷题记录

---
+++ 剑指Offer/剑指 Offer 32 - I. 从上到下打印二叉树.py
@@ -0,0 +1,17 @@
+import collections
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res, queue = [], collections.deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            res.append(node.val)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return res
