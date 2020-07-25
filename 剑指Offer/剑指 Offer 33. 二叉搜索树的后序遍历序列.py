e90d252f37abd7efe3c7150e6b74c9c85d422cd4
PlutoaCharon <thisishaohan@foxmail.com>
2020-07-25 21:36:59 +0800

    Python刷题记录

---
+++ 剑指Offer/剑指 Offer 33. 二叉搜索树的后序遍历序列.py
@@ -0,0 +1,10 @@
+class Solution:
+    def verifyPostorder(self, postorder: List[int]) -> bool:
+        stack, root = [], float("inf")
+        for i in range(len(postorder) -1 , -1, -1):
+            if postorder[i] > root:
+                return False
+            while stack and postorder[i] < stack[-1]:
+                root = stack.pop()
+            stack.append(postorder[i])
+        return True
---
+++ 剑指Offer/剑指 Offer 34. 二叉树中和为某一值的路径.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def pathSum(self, root: TreeNode, sum: int) -> List[List[int]]:
+        if not root: return []
+
+        res, path = [], []
+        def recur(root, tar):
+            if not root:
+                return
+            path.append(root.val)
+            tar -= root.val
+            if tar == 0 and not root.left and not root.right:
+                res.append(list(path))
+            recur(root.left, tar)
+            recur(root.right, tar)
+            path.pop()
+        recur(root, sum)
+        return res
