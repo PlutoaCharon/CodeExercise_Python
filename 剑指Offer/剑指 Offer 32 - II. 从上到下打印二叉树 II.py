95c5a07d4c4dd16d8bd95980197af3f54b3a3680
PlutoaCharon <thisishaohan@foxmail.com>
2020-07-25 20:36:43 +0800

    Python刷题记录

---
+++ 剑指Offer/剑指 Offer 32 - II. 从上到下打印二叉树 II.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        res, queue = [], [root]
+        while queue:
+            size = len(queue)
+            out = []
+            for i in range(size):
+                r = queue.pop(0)
+                out.append(r.val)
+                if r.left:
+                    queue.append(r.left)
+                if r.right:
+                    queue.append(r.right)
+            res.append(out)
+        return res
