7564d959e3068fb5f55780ad96d0379b476d107b
PlutoaCharon <thisishaohan@foxmail.com>
2020-08-02 16:45:27 +0800

    Python刷题记录

---
+++ LeetCode/144. 二叉树的前序遍历.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# 递归
+# 时间复杂度：O(n)，n为节点数，访问每个节点恰好一次。
+# 空间复杂度：空间复杂度：O(h)，h为树的高度。最坏情况下需要空间O(n)，平均情况为O(logn)
+# 递归1：二叉树遍历最易理解和实现版本
+
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        return [root.val] + self.preorderTraversal(root.left) + self.preorderTraversal(root.right)
+
+
+# 迭代
+# 时间复杂度：O(n)，n为节点数，访问每个节点恰好一次。
+# 空间复杂度：O(h)，h为树的高度。取决于树的结构，最坏情况存储整棵树，即O(n)
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res, stack, cur = [], [], root
+        while stack or cur:
+            while cur:
+                res.append(cur.val)
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            cur = cur.right
+        return res
---
+++ LeetCode/94. 二叉树的中序遍历.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+# 递归
+# 时间复杂度：O(n)，n为节点数，访问每个节点恰好一次。
+# 空间复杂度：空间复杂度：O(h)，h为树的高度。最坏情况下需要空间O(n)，平均情况为O(logn)
+# 递归1：二叉树遍历最易理解和实现版本
+
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] +  self.inorderTraversal(root.right)
+
+# 迭代
+# 时间复杂度：O(n)，n为节点数，访问每个节点恰好一次。
+# 空间复杂度：O(h)，h为树的高度。取决于树的结构，最坏情况存储整棵树，即O(n)
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        res, stack, cur = [], [], root
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            res.append(cur.val)
+            cur = cur.right
+        return res
