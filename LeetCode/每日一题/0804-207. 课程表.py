9b91addbd1ef207719448889fd1a835191d5aabb
PlutoaCharon <thisishaohan@foxmail.com>
2020-08-04 11:01:22 +0800

    Python刷题记录

---
+++ LeetCode/每日一题/0804-207. 课程表.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        indegrees = [0 for _ in range(numCourses)]
+        adjacency = [[] for _ in range(numCourses)]
+        queue = deque()
+        # Get the indegree and adjacency of every course.
+        for cur, pre in prerequisites:
+            indegrees[cur] += 1
+            adjacency[pre].append(cur)
+        # Get all the courses with the indegree of 0.
+        for i in range(len(indegrees)):
+            if not indegrees[i]:
+                queue.append(i)
+        # BFS TopSort.
+        while queue:
+            pre = queue.popleft()
+            numCourses -= 1
+            for cur in adjacency[pre]:
+                indegrees[cur] -= 1
+                if not indegrees[cur]:
+                    queue.append(cur)
+        return not numCourses
