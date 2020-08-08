ab0fa366d001dd8e35af0bf8581a263c7f9215ea
PlutoaCharon <thisishaohan@foxmail.com>
2020-08-08 19:25:07 +0800

    Python刷题记录

--- 剑指Offer/剑指 Offer 41. 数据流中的中位数.py
+++ 剑指Offer/剑指 Offer 41. 数据流中的中位数.py
@@ -1,9 +1,10 @@
 from heapq import *
 
+
 class MedianFinder:
     def __init__(self):
-        self.A = [] # 小顶堆，保存较大的一半
-        self.B = [] # 大顶堆，保存较小的一半
+        self.A = []  # 小顶堆，保存较大的一半
+        self.B = []  # 大顶堆，保存较小的一半
 
     def addNum(self, num: int) -> None:
         if len(self.A) != len(self.B):
@@ -15,7 +16,6 @@ class MedianFinder:
 
     def findMedian(self) -> float:
         return self.A[0] if len(self.A) != len(self.B) else (self.A[0] - self.B[0]) / 2.0
-
 
 # Your MedianFinder object will be instantiated and called as such:
 # obj = MedianFinder()
