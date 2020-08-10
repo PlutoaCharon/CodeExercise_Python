388be29f1b3a13ab7cde5557f9b963ecf088806d
PlutoaCharon <thisishaohan@foxmail.com>
2020-08-10 18:39:28 +0800

    Python刷题记录

---
+++ LeetCode/面试题 01.07. 旋转矩阵.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # matrix[:] = list(map(list, zip(*(matrix[::-1]))))
+        n = len(matrix)
+        matrixNew = [[0] * n for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                matrixNew[j][n - i -1] = matrix[i][j]
+        matrix[:] = matrixNew
