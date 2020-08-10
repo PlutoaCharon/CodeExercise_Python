f5bf643b614e4ad869a8bdf64de483a1e7c275ac
PlutoaCharon <thisishaohan@foxmail.com>
2020-08-10 13:33:48 +0800

    Python刷题记录

--- 剑指Offer/剑指 Offer 10- II. 青蛙跳台阶问题.py
+++ 剑指Offer/剑指 Offer 10- II. 青蛙跳台阶问题.py
@@ -7,8 +7,17 @@ class Solution:
             return 1
         if n == 2:
             return 2
-        for _ in range(3, n+1):
+        for _ in range(3, n + 1):
             c = a + b
             a = b
             b = c
         return c % 1000000007
+
+
+class Solution:
+    def Fibonacci(self, n):
+        # write code here
+        a, b = 0, 1
+        for i in range(0, n):
+            a, b = b, a + b
+        return a
