d9d323f5de53be71c36835c1bffd09fd0eadeb23
PlutoaCharon <thisishaohan@foxmail.com>
2020-08-10 18:51:13 +0800

    Python刷题记录

--- LeetCode/48. 旋转图像.py
+++ LeetCode/48. 旋转图像.py
@@ -10,3 +10,18 @@ class Solution:
             for j in range(n):
                 matrixNew[j][n - i - 1] = matrix[i][j]
         matrix[:] = matrixNew
+
+
+# 原地置换：
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        # 先转置 后镜像
+        row, col = len(matrix), len(matrix[0])
+        for i in range(row - 1):
+            for j in range(i + 1, col):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(row):
+            matrix[i] = matrix[i][::-1]
