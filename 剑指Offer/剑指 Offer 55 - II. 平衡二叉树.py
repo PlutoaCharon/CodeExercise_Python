10e47252463699c68d3175c3dc7a355f82346ec0
PlutoaCharon <thisishaohan@foxmail.com>
2020-08-17 18:17:36 +0800

    Python刷题记录

---
+++ 剑指Offer/剑指 Offer 55 - II. 平衡二叉树.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.recur(root) != -1
+
+    def recur(self, root):
+        if not root:
+            return 0
+        left = self.recur(root.left)
+        if left == -1:
+            return -1
+        right = self.recur(root.right)
+        if right == -1:
+            return -1
+        return max(left, right) + 1 if abs(left - right) < 2 else -1
